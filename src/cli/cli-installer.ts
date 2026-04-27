ee463f76be33c111ac995dc48e483af8d8a68796
sheltonsuen <sheltonsuen@gmail.com>
2026-04-27 14:22:46 +0800

    chore: refine

--- src/cli/cli-installer.ts
+++ src/cli/cli-installer.ts
@@ -202,12 +202,6 @@ export async function runCliInstaller(
     "The Magic Word",
   )
 
-  console.log(
-    `${SYMBOLS.star} ${color.yellow("If you found this helpful, consider starring the repo!")}`,
-  )
-  console.log(
-    `  ${color.dim("gh api --silent --method PUT /user/starred/code-yeongyu/oh-my-openagent >/dev/null 2>&1 || true")}`,
-  )
   console.log()
   console.log(color.dim("oMoMoMoMo... Enjoy!"))
   console.log()
