f0488765df14349446f4909e50c9bed773c35152
sheltonsuen <sheltonsuen@gmail.com>
2026-05-06 10:26:30 +0800

    chore: add url

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -4,4 +4,7 @@ export default defineConfig({
   dialect: "sqlite",
   schema: "./src/features/database/schema.ts",
   out: "./src/features/database/migrations",
+  dbCredentials: {
+    url: "file:oh-my-codes.db",
+  },
 })
